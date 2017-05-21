043f543e01ad6d84c613892f35750ee99bde1f73
Łukasz S. <lsajenko121@gmail.com>
2017-05-21 14:07:54 +0300

    Czwarta wersja

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.time.LocalDate;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Teraz sprawdzam system zapisu po zakominutowaniu w gicie");
 		System.out.println("Coś jeszcze dodatkowego teraz, bo alarm był");
+		System.out.println(LocalDate.now());
 
 	}
 
