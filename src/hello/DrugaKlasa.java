ad6b9a33db4cbdc53bbff0de1a469b7001f70a8b
Łukasz S. <lsajenko121@gmail.com>
2017-05-21 14:06:56 +0300

    Trzecia wersja

--- src/hello/DrugaKlasa.java
+++ src/hello/DrugaKlasa.java
@@ -3,6 +3,7 @@ package hello;
 public class DrugaKlasa {
 
 	public static void main(String[] args) {
+		System.out.println("Trzeba jeszcze coś dodać");
 		System.out.println("hej");
 		// TODO Auto-generated method stub
 
