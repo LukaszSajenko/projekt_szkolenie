c42a12f64a7c9ad1aa9b7e76d55d5a81f2e1c6d8
Łukasz S. <lsajenko121@gmail.com>
2017-05-21 13:27:53 +0300

    Kolejna wersja po dodaniu jeszcze jednej linijki

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
+		System.out.println("Teraz sprawdzam system zapisu po zakominutowaniu w gicie");
 
 	}
 
